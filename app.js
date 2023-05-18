3d81f83dacf5e7ddc81e34d8dad8b2777b1647e7
unknown <becario@EII-D104-00.aulasuo.uniovi.es>
2023-05-18 15:26:10 +0200

    login y user router, generate apiKey

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ let express = require("express")
 let app = express()
 let port = 8081
 
+app.use(express.json())
+
 let routerItems = require("./routers/routerItems")
 let routerBids = require("./routers/routeBids")
+let routerUsers = require("./routers/routerUsers")
+
 app.use("/items",routerItems)
 app.use("/bids",routerBids)
+app.use("/users",routerUsers)
 
 app.listen(port, () => {
     console.log("Servidor activo en "+port)
